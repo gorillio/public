a7b0f98adf0f01a8451f3a8e66c5acc7677840ce
adheip <adheip1222@gmail.com>
2020-07-09 19:03:57 +0530

    minikube script

--- scripts/travis/minikube.sh
+++ scripts/travis/minikube.sh
@@ -4,12 +4,16 @@ echo "==========================================================================
 echo " minikube setup"
 echo "==============================================================================================================="
 
- # Download minikube.
-- curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.8.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-- mkdir -p $HOME/.kube $HOME/.minikube
-- touch $KUBECONFIG
-- sudo minikube start --profile=minikube --vm-driver=none --kubernetes-version=v1.18.1
-- minikube update-context --profile=minikube
-- "sudo chown -R travis: /home/travis/.minikube/"
-- eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
-- JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n kube-system get pods -lk8s-app=kube-dns -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for kube-dns to be available"; kubectl get pods --all-namespaces; done
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.8.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+mkdir -p $HOME/.kube $HOME/.minikube
+touch $KUBECONFIG
+sudo minikube start --profile=minikube --vm-driver=none --kubernetes-version=v1.18.1
+minikube update-context --profile=minikube
+"sudo chown -R travis: /home/travis/.minikube/"
+eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
+  until kubectl -n kube-system get pods -lk8s-app=kube-dns -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; \
+  do sleep 1; \
+  echo "waiting for kube-dns to be available"; \ 
+  kubectl get pods --all-namespaces; \
+  done;
